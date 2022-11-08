3f8254ca4fcba2ec7fbde471adea370a8def4a5e
kuu13580 <46004336+kuu13580@users.noreply.github.com>
2022-11-08 14:47:05 +0900

    create readme

--- js/question4.js
+++ js/question4.js
@@ -5,18 +5,16 @@ $(function(){
   
   if (!localStorage.hasOwnProperty("reload_count_2")) {
     localStorage.setItem("reload_count_2", 1);
-    $(".first-message").fadeTo(1000,1);
   } else {
     localStorage.setItem("reload_count_2", Number(localStorage.getItem("reload_count_2")) + 1);
     (async () => {
       console.time('Waited for');
       await new Promise(resolve => setTimeout(resolve, 5000));
       console.timeLog('Waited for');
-      if (localStorage.getItem("reload_count_2") == 10) {
+      if (localStorage.getItem("reload_count_2") == 7) {
         $(".answer").css("right", - window.innerWidth / 2 + "px");
       }
       localStorage.setItem("reload_count_2", 1);
-      $(".first-message").fadeTo(1000,1);
     })();
   }
 
